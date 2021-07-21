3750f948eafc76f7e4865097582d4e55b7673e42
choihyerim <hyerimc858@gmail.com>
2021-07-21 18:35:35 +0900

    INITIAL SETTINGS

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wrapup',
+        'USER': 'root',
+        'PASSWORD': 'kim99108!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'b5o_rbr_7jr+vd2bkpxc3prvfw=qyl6c=0yl_p=*rjm!)5f37^'
